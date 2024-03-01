417ff1289ea8de259c9705002e2c3f5425ab2517
sonyakun <owner@sonyakun.com>
2024-03-01 17:02:30 +0900

    色々追加

--- misspy/__init__.py
+++ misspy/__init__.py
@@ -1,9 +1,9 @@
 from importlib.metadata import version 
 
-from .Bot import Bot
-from .settings import Option, extension
+from .Bot import Bot # noqa: F401
+from .settings import Option, extension  # noqa: F401
 
-from .core.types import *
+from .core.types import *  # noqa: F403
 from .core.experimental import aiows
 from .core import websocket
 
@@ -11,3 +11,15 @@ __version__ = version("misspy")
 
 MSC = aiows.MSC # Misskey
 MIWS_V2 = websocket.MiWS_V2
+
+homeTimeline = "homeTimeline"
+localTimeline = "localTimeline"
+socialTimeline = "hybridTimeline"
+hybridTimeline = "hybridTimeline"
+globalTimeline = "globalTimeline"
+
+class visibility:
+    public = "public"
+    home = "home"
+    followers = "followers"
+    specified = "specified"
