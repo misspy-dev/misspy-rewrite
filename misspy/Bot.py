1a390c6a3e6dfd92c42846c476f0411db50be6a2
sonyakun <owner@sonyakun.com>
2024-03-01 19:35:19 +0900

    Update Bot.py

--- misspy/Bot.py
+++ misspy/Bot.py
@@ -1,24 +1,41 @@
 import asyncio
 from typing import Union
+import logging
+import re
 
+from .core.http import AsyncHttpHandler
 from .core.types.note import Note
-from .settings import Option
-from .settings import extension
+from .endpoints.drive import drive
+from .endpoints.notes import notes
+from .settings import Option, extension
 
 
 class Bot:
     def __init__(
         self,
         address: str,
-        i: Union[str, None],
+        i: Union[str, None]
     ) -> None:
+        logger = logging.getLogger("misspy")
         self.address = address
         self.i = i
         self.ssl = Option.ssl
         self.ext = extension
+        self.http = AsyncHttpHandler(self.address, self.i, self.ssl)
+
+        self.endpoint_list = self.endpoints()
+        # ---------- endpoints ------------
+        self.notes = notes(self.address, self.i, self.ssl, endpoints=self.endpoint_list)
+        self.drive = drive(self.address, self.i, self.ssl, endpoints=self.endpoint_list)
+        # ---------------------------------
+
+    async def endpoints(self):
+        return await self.http.send("endpoints", data={})
 
     def run(self, reconnect=False):
-        self.ws = Option.ws_engine(self.address, self.i, self.handler, reconnect, self.ssl)
+        self.ws = Option.ws_engine(
+            self.address, self.i, self.handler, reconnect, self.ssl
+        )
         asyncio.run(self.ws.start())
 
     async def connect(self, channel, id=None):
@@ -30,7 +47,7 @@ class Bot:
                 pnote = Note(**json["body"]["body"])
                 for func in extension.exts["note"]:
                     await func(pnote)
-            if json['body']['type'] == 'followed':
+            if json["body"]["type"] == "followed":
                 for func in extension.exts["followed"]:
                     await func()
         elif json["type"] == "__internal":
