9613261496aa7e36e8b5d558d9eccc4f0673d278
sonyakun <owner@sonyakun.com>
2024-05-02 21:30:00 +0900

    いろいろ

--- misspy/core/types/note.py
+++ misspy/core/types/note.py
@@ -1,16 +1,13 @@
-from typing import Union, List, Any
-from mitypes.user import AvatarDecorations
+from typing import List, Union
 
-from pydantic import dataclasses, Field
-
-from mitypes.user import UserLite
 from mitypes.drive import DriveFile
+from mitypes.user import UserLite
+from pydantic import Field, dataclasses
 
-from .internal import mspy
 from .action import APIAction
-from .page import Page
-from .federation import Instance, Emojis, BadgeRoles, field
-from .role import RoleLite
+from .channel import Channel
+from .internal import mspy
+
 
 @dataclasses.dataclass(config=dict(extra="allow"))
 class Context:
@@ -18,35 +15,35 @@ class Context:
     createdAt: str
     userId: str
     user: UserLite
-    replyId: Union[str, None]
-    renoteId: Union[str, None]
+    replyId: Union[str, None] = Field(default_factory=None)
+    renoteId: Union[str, None] = Field(default_factory=None)
     visibility: str
     localOnly: bool
-
-    reactions: Union[dict, None] = None
-    myReaction: Union[dict, None] = None
-    uri: Union[str, None] = None
-    url: Union[str, None] = None
-    renoteCount: Union[int, None] = None
-    replyesCount: Union[int, None] = None
-    isHidden: Union[bool, None] = None
-    deletedAt: Union[str, None] = None
-    text: Union[str, None] = None
-    cw: Union[str, None] = None
-    reply: Union[dict, None] = None
-    renote: Union[dict, None] = None
+    reactions: Union[dict, None] = Field(default_factory=None)
+    myReaction: Union[dict, None] = Field(default_factory=None)
+    uri: Union[str, None] = Field(default_factory=None)
+    url: Union[str, None] = Field(default_factory=None)
+    renoteCount: Union[int, None] = Field(default_factory=None)
+    replyesCount: Union[int, None] = Field(default_factory=None)
+    isHidden: Union[bool, None] = Field(default_factory=None)
+    deletedAt: Union[str, None] = Field(default_factory=None)
+    text: Union[str, None] = Field(default_factory=None)
+    cw: Union[str, None] = Field(default_factory=None)
+    reply: Union[dict, None] = Field(default_factory=None)
+    renote: Union[dict, None] = Field(default_factory=None)
     mentions: List[str] = Field(default_factory=List)
     visibleUserIds: List[str] = Field(default_factory=List)
     fileIds: List[str] = Field(default_factory=List)
     files: List[DriveFile] = Field(default_factory=List)
     tags: List[str] = Field(default_factory=List)
-    poll: Union[dict, None] = None
-    channelId: Union[str, None] = None
-    channel: Union[dict, None] = None
-    reactionAcceptance: Union[str, None] = None
+    poll: Union[dict, None] = Field(default_factory=None)
+    channelId: Union[str, None] = Field(default_factory=None)
+    channel: Union[dict, None] = Field(default_factory=None)
+    reactionAcceptance: Union[str, None] = Field(default_factory=None)
     reactionAndUserPairCache: List[str] = Field(default_factory=List)
-    misspy: Union[mspy, None] = None
-    api: Union[APIAction, None] = None
+    misspy: Union[mspy, None] = Field(default_factory=None)
+    api: Union[APIAction, None] = Field(default_factory=None)
+
 
 @dataclasses.dataclass(config=dict(extra="allow"))
 class Note:
@@ -54,147 +51,81 @@ class Note:
     createdAt: str
     userId: str
     user: UserLite
-    replyId: Union[str, None]
-    renoteId: Union[str, None]
+    replyId: Union[str, None] = Field(default_factory=None)
+    renoteId: Union[str, None] = Field(default_factory=None)
     visibility: str
     localOnly: bool
-    reactions: Union[dict, None] = None
-    myReaction: Union[dict, None] = None
-    uri: Union[str, None] = None
-    url: Union[str, None] = None
-    renoteCount: Union[int, None] = None
-    replyesCount: Union[int, None] = None
-    isHidden: Union[bool, None] = None
-    deletedAt: Union[str, None] = None
-    text: Union[str, None] = None
-    cw: Union[str, None] = None
-    reply: Union[dict, None] = None
-    renote: Union[dict, None] = None
+    reactions: Union[dict, None] = Field(default_factory=None)
+    myReaction: Union[dict, None] = Field(default_factory=None)
+    uri: Union[str, None] = Field(default_factory=None)
+    url: Union[str, None] = Field(default_factory=None)
+    renoteCount: Union[int, None] = Field(default_factory=None)
+    replyesCount: Union[int, None] = Field(default_factory=None)
+    isHidden: Union[bool, None] = Field(default_factory=None)
+    deletedAt: Union[str, None] = Field(default_factory=None)
+    text: Union[str, None] = Field(default_factory=None)
+    cw: Union[str, None] = Field(default_factory=None)
+    reply: Union[dict, None] = Field(default_factory=None)
+    renote: Union[dict, None] = Field(default_factory=None)
     mentions: List[str] = Field(default_factory=List)
     visibleUserIds: List[str] = Field(default_factory=List)
     fileIds: List[str] = Field(default_factory=List)
     files: List[DriveFile] = Field(default_factory=List)
     tags: List[str] = Field(default_factory=List)
-    poll: Union[dict, None] = None
-    channelId: Union[str, None] = None
-    channel: Union[dict, None] = None
-    reactionAcceptance: Union[str, None] = None
+    poll: Union[dict, None] = Field(default_factory=None)
+    channelId: Union[str, None] = Field(default_factory=None)
+    channel: Union[Channel, None] = Field(default_factory=None)
+    reactionAcceptance: Union[str, None] = Field(default_factory=None)
     reactionAndUserPairCache: List[str] = Field(default_factory=List)
-    misspy: Union[mspy, None] = None
+    misspy: Union[mspy, None] = Field(default_factory=None)
 
 
-
-@dataclasses.dataclass()
-class SecurityKeysList:
-    id: str
-    name: str
-    lastUsed: str
-
-@dataclasses.dataclass()
-class RolePolicies:
-    gtlAvailable: bool
-    ltlAvailable: bool
-    canPublicNote: bool
-    canInitiateConversation: bool
-    canCreateContent: bool
-    canUpdateContent: bool
-    canDeleteContent: bool
-    canPurgeAccount: bool
-    canUpdateAvatar: bool
-    canUpdateBanner: bool
-    mentionLimit: int
-    canInvite: bool
-    inviteLimit: int
-    inviteLimitCycle: int
-    inviteExpirationTime: int
-    canManageCustomEmojis: bool
-    canManageAvatarDecorations: bool
-    canSearchNotes: bool
-    canUseTranslator: bool
-    canUseDriveFileInSoundSettings: bool
-    canHideAds: bool
-    driveCapacityMb: int
-    alwaysMarkNsfw: bool
-    skipNsfwDetection: bool
-    pinLimit: int
-    antennaLimit: int
-    antennaNotesLimit: int
-    wordMuteLimit: int
-    webhookLimit: int
-    clipLimit: int
-    noteEachClipsLimit: int
-    userListLimit: int
-    userEachUserListsLimit: int
-    rateLimitFactor: int
-    avatarDecorationLimit: int
-
-@dataclasses.dataclass()
-class Achievements:
-    name: str
-    unlockedAt: int
-
-@dataclasses.dataclass()
-class Announcement:
-    id: str
-    createdAt: str
-    updatedAt: Union[str, None]
-    text: str
-    title: str
-    imageUrl: Union[str, None]
-    icon: str
-    display: str
-    needConfirmationToRead: bool
-    forYou: bool
-    closeDuration: int
-    displayOrder: int
-    silence: bool
-    isRead: bool
-
+"""
 @dataclasses.dataclass()
 class Pin:
     id: str
-    name: Union[str, None]
-    username: str
-    host: Union[str, None]
-    avatarUrl: Union[str, None]
-    avatarBlurhash: Union[str, None]
-    avatarDecorations: AvatarDecorations
-    isBot: bool
-    isCat: bool
-    instance: Instance
-    emojis: Emojis
-    onlineStatus: str
-    badgeRoles: List[BadgeRoles]
-    url: Union[str, None]
-    uri: Union[str, None]
-    movedTo: Union[str, None]
-    alsoKnownAs: List[Union[str, None]]
+    name: Union[str, None] = Field(default_factory=None)
+    username: Union[str, None] = Field(default_factory=None)
+    host: Union[str, None] = Field(default_factory=None)
+    avatarUrl: Union[str, None] = Field(default_factory=None)
+    avatarBlurhash: Union[str, None] = Field(default_factory=None)
+    avatarDecorations: List[AvatarDecorations] = Field(default_factory=List)
+    isBot: Union[bool, None] = Field(default_factory=None)
+    isCat: Union[bool, None] = Field(default_factory=None)
+    instance: Union[Instance, None] = Field(default_factory=None)
+    emojis: Emojis = Field(default_factory=List)
+    onlineStatus: str = Field(default_factory="unknown")
+    badgeRoles: List[BadgeRoles] = Field(default_factory=list)
+    url: Union[str, None] = Field(default_factory=None)
+    uri: Union[str, None] = Field(default_factory=None)
+    movedTo: Union[str, None] = Field(default_factory=None)
+    alsoKnownAs: List[Union[str, None]] = Field(default_factory=None)
     createdAt: str
-    updatedAt: Union[str, None]
-    lastFetchedAt: Union[str, None]
-    bannerUrl: Union[str, None]
-    bannerBlurhash: Union[str, None]
+    updatedAt: Union[str, None] = Field(default_factory=None)
+    lastFetchedAt: Union[str, None] = Field(default_factory=None)
+    bannerUrl: Union[str, None] = Field(default_factory=None)
+    bannerBlurhash: Union[str, None] = Field(default_factory=None)
     isLocked: bool
     isSilenced: bool
     isLimited: bool
     isSuspended: bool
-    description: Union[str, None]
-    location: Union[str, None]
-    birthday: Union[str, None]
-    lang: Union[str, None]
-    fields: List[field]
-    verifiedLinks: List[str]
+    description: Union[str, None] = Field(default_factory=None)
+    location: Union[str, None] = Field(default_factory=None)
+    birthday: Union[str, None] = Field(default_factory=None)
+    lang: Union[str, None] = Field(default_factory=None)
+    fields: List[field] = Field(default_factory=list)
+    verifiedLinks: List[str] = Field(default_factory=list)
     followersCount: int
     followingCount: int
     notesCount: int
-    pinnedNoteIds: List[str]
-    pinnedNotes: List[Note]
-    pinnedPageId: Union[str, None]
+    pinnedNoteIds: List[str] = Field(default_factory=list)
+    pinnedNotes: List[Note] = Field(default_factory=list)
+    pinnedPageId: Union[str, None] = Field(default_factory=None)
     pinnedPage: Page
     publicReactions: bool
     followingVisibility: str
     followersVisibility: str
-    memo: Union[str, None]
+    memo: Union[str, None] = Field(default_factory=None)
     moderationNote: str
     isFollowing: bool
     isFollowed: bool
@@ -206,10 +137,10 @@ class Pin:
     isRenoteMuted: bool
     notify: str
     withReplies: bool
-    avatarId: Union[str, None]
-    bannerId: Union[str, None]
-    isModerator: Union[bool, None]
-    isAdmin: Union[bool, None]
+    avatarId: Union[str, None] = Field(default_factory=None)
+    bannerId: Union[str, None] = Field(default_factory=None)
+    isModerator: Union[bool, None] = Field(default_factory=None)
+    isAdmin: Union[bool, None] = Field(default_factory=None)
     injectFeaturedNote: bool
     receiveAnnouncementEmail: bool
     alwaysMarkNsfw: bool
@@ -231,17 +162,18 @@ class Pin:
     hasUnreadNotification: bool
     hasPendingReceivedFollowRequest: bool
     unreadNotificationsCount: int
-    mutedWords: List[str]
-    mutedInstances: List[Union[str, None]]
+    mutedWords: List[str] = Field(default_factory=list)
+    mutedInstances: List[Union[str, None]] = Field(default_factory=None)
     notificationRecieveConfig: Any
-    emailNotificationTypes: List[str]
-    achievements: List[Achievements]
+    emailNotificationTypes: List[str] = Field(default_factory=list)
+    achievements: List[Achievements] = Field(default_factory=list)
     loggedInDays: int
     policies: RolePolicies
     role: RoleLite
-    email: Union[str, None]
-    emailVerified: Union[bool, None]
-    securityKeysList: List[SecurityKeysList]
+    email: Union[str, None] = Field(default_factory=None)
+    emailVerified: Union[bool, None] = Field(default_factory=None)
+    securityKeysList: List[SecurityKeysList] = Field(default_factory=list)
     twoFactorEnabled: bool = False
     usePasswordLessLogin: bool = False
     securityKeys: bool = False
+"""
