cd241574a3417181d89e72c691a7dab2c492eaae
sonyakun <owner@sonyakun.com>
2024-05-02 21:29:49 +0900

    misspy.Timeline追加 (Enum化)

--- misspy/__init__.py
+++ misspy/__init__.py
@@ -4,7 +4,8 @@ from mitypes.drive import DriveFolder, df_property, DriveFile
 from mitypes.user import AvatarDecorations, User, UserLite
 from mitypes.poll import Poll
 
-from .Bot import Bot
+from .Bot import Bot, Timeline
+from .Cog import Cog
 
 from .core import websocket
 from .core.experimental import aiows
@@ -15,15 +16,3 @@ __version__ = version("misspy")
 
 MSC = aiows.MSC  # Misskey
 MIWS_V2 = websocket.MiWS_V2
-
-homeTimeline = "homeTimeline"
-localTimeline = "localTimeline"
-socialTimeline = "hybridTimeline"
-hybridTimeline = "hybridTimeline"
-globalTimeline = "globalTimeline"
-
-class visibility:
-    public = "public"
-    home = "home"
-    followers = "followers"
-    specified = "specified"
